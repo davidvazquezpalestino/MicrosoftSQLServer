b4b2bb984e583908c8e1f54c5467f1ccb94c4965
David Vázquez Palestino <76629621+davidvazquezpalestino@users.noreply.github.com>
2025-05-14 00:37:51 -0600

    Update LayoutSalarios.sql

--- NÓMINA/MEGA/LayoutSalarios.sql
+++ NÓMINA/MEGA/LayoutSalarios.sql
@@ -1,6 +1,6 @@
 
 SELECT *
---	UPDATE SalarioAnterior SET SalarioAnterior.FechaFin = '2025-02-28'
+--	UPDATE SalarioAnterior SET SalarioAnterior.FechaFin = '2025-04-30'
 FROM dbo.tNOMempleadosSalarios SalarioAnterior
 INNER JOIN dbo.vPERempleados empleado ON empleado.IdEmpleado = SalarioAnterior.IdEmpleado
 INNER JOIN ##Salarios SalarioNuevo ON empleado.Codigo COLLATE DATABASE_DEFAULT = SalarioNuevo.Codigo 
@@ -8,10 +8,10 @@ WHERE SalarioAnterior.FechaFin = '1900-01-01' AND SalarioAnterior.IdEmpresaNomin
 	
 
 INSERT INTO dbo.tNOMempleadosSalarios (IdEmpresaNomina, IdEmpleado, FechaInicio, FechaFin, SalarioDiario, BaseCotizar, SDI )
-SELECT SalarioAnterior.IdEmpresaNomina, empleado.IdEmpleado, FechaInicio = '2025-03-01', FechaFin = '1900-01-01', Salario.sd, BaseCotizar = ROUND(ISNULL(Salario.sdi, 0), 2), SDI = ROUND(ISNULL(Salario.sdi, 0), 2)
+SELECT SalarioAnterior.IdEmpresaNomina, empleado.IdEmpleado, FechaInicio = '2025-05-01', FechaFin = '1900-01-01', Salario.sd, BaseCotizar = ROUND(ISNULL(Salario.sdi, 0), 2), SDI = ROUND(ISNULL(Salario.sdi, 0), 2)
 FROM dbo.tNOMempleadosSalarios SalarioAnterior
 INNER JOIN dbo.vPERempleados empleado ON empleado.IdEmpleado = SalarioAnterior.IdEmpleado
 INNER JOIN ##Salarios Salario ON empleado.Codigo COLLATE DATABASE_DEFAULT = Salario.Codigo 
-WHERE SalarioAnterior.FechaFin = '2025-02-28' AND SalarioAnterior.IdEmpresaNomina <> 3
+WHERE SalarioAnterior.FechaFin = '2025-04-30' AND SalarioAnterior.IdEmpresaNomina <> 3
 
 
